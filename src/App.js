00178d06882ae494bab31d776ecbb703ee7acfe2
papamasro <54428071+papamasro@users.noreply.github.com>
2021-04-24 23:22:39 -0300

    Add files via upload
    FIRST PUSH

---
+++ src/App.js
@@ -0,0 +1,198 @@
+import './App.css';
+import React from 'react'
+import { Button, Card, Image, Segment, Radio, Icon, Header } from 'semantic-ui-react'
+
+
+import aire from './images/aire.jpeg';
+
+import compu from './images/computadora.jpeg';
+
+import pasto from './images/cortapasto.png';
+
+import faroles from './images/faroles.jpeg';
+
+import lava from './images/lavaropas.jpeg';
+
+import heladera from './images/heladera.jpg';
+
+import pileta from './images/pileta.jpeg';
+
+import tv from './images/tv.jpeg';
+
+
+function App() {
+  return (
+    <Segment>
+      <Header as='h2' attached='top'>
+        <Icon name='home' />
+      Casa 1
+    </Header>
+      <Segment attached color='red' >
+        <Card.Group> 
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={heladera}
+              />
+              <Card.Header>Heladera</Card.Header>
+              <Card.Meta>Consumo: 95 Wh</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={aire}
+              />
+              <Card.Header>Aire</Card.Header>
+              <Card.Meta>Consumo: 1048 Wh</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={compu}
+              />
+              <Card.Header>PC</Card.Header>
+              <Card.Meta>Consumo: 650 Wh</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Segment>
+      <Header as='h2' attached='top'>
+        <Icon name='home' />
+      Casa 2
+    </Header>
+      <Segment attached color='blue' >
+        <Card.Group>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={heladera}
+              />
+              <Card.Header>Heladera</Card.Header>
+              <Card.Meta>Consumo: 95 Wh</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={lava}
+              />
+              <Card.Header>Lavarropas</Card.Header>
+              <Card.Meta>Consumo: 875 Wh</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={tv}
+              />
+              <Card.Header>Televisor</Card.Header>
+              <Card.Meta>Consumo: 120Wh</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Segment>
+      <Header as='h2' attached='top'>
+        <Icon name='warehouse' />
+      Sector Comun
+    </Header>
+      <Segment attached color='green' >
+        <Card.Group>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={faroles}
+              />
+              <Card.Header>Iluminacion</Card.Header>
+              <Card.Meta>Consumo: 150Wh x 20</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={pileta}
+              />
+              <Card.Header>Motor pileta</Card.Header>
+              <Card.Meta>Consumo: 570Wh x 2</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='tiny'
+                src={pasto}
+              />
+              <Card.Header>Cortadoras de pasto</Card.Header>
+              <Card.Meta>Consumo: 550Wh x 3</Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='power off' size="large" color="red" />
+              <Radio toggle />
+              <Icon name='plug' size="large" color="green" />
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Segment>
+    </Segment>
+  );
+}
+
+export default App;
